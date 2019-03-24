bb4773bc217d93ab3c488aa7570fb3313fb823ad
Unknown <ac.gonzalesllerena@gmail.com>
2019-03-24 16:24:20 -0500

    Personalización de response

--- src/main/java/com/aws/codestart/projecttemplates/controller/ClientController.java
+++ src/main/java/com/aws/codestart/projecttemplates/controller/ClientController.java
@@ -17,7 +17,6 @@ import com.aws.codestart.projecttemplates.controller.request.ClientRequest;
 import com.aws.codestart.projecttemplates.controller.response.KpiClient;
 import com.aws.codestart.projecttemplates.controller.response.Response;
 import com.aws.codestart.projecttemplates.domain.Client;
-import com.aws.codestart.projecttemplates.exception.ClientInsertNullException;
 import com.aws.codestart.projecttemplates.service.ClientService;
 
 
@@ -35,31 +34,28 @@ public class ClientController {
     
     @PostMapping("/creacliente")
     public ResponseEntity<Response>  create(@Valid @RequestBody ClientRequest request) {
-    	try {
-    		this.clientService.create(request.getClient());
-		} catch (Exception e) {
-			 throw new ClientInsertNullException("Error al registrar el visitante");
-		}
-    	return new ResponseEntity<Response>(new Response(request,"registrado correctamente"),HttpStatus.CREATED); 
+    	
+		this.clientService.create(request.getClient());
+    	return new ResponseEntity<Response>(new Response(request,"Registrado correctamente"),HttpStatus.CREATED); 
     }
     
     
     @GetMapping("/kpideclientes")
-    public ResponseEntity<KpiClient>  findKpi() {    	
+    public ResponseEntity<Response>  findKpi() {    	
     	KpiClient kpiClient =  this.clientService.findKpi();
 		if (kpiClient == null) {
 			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 		}
-    	return new ResponseEntity<KpiClient>(kpiClient,HttpStatus.OK); 
+    	return new ResponseEntity<Response>(new Response(kpiClient,"Procesado correctamente"),HttpStatus.OK); 
     }
     
     @GetMapping("/listclientes")
-    public ResponseEntity<List<Client>>  findDocument() {
+    public ResponseEntity<Response>  findDocument() {
     	List<Client> lstClient =  this.clientService.findAll();
 		if (lstClient == null) {
 			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 		}
-    	return new ResponseEntity<List<Client>>(lstClient,HttpStatus.OK); 
+    	return new ResponseEntity<Response>(new Response(lstClient,"Procesado correctamente"),HttpStatus.OK); 
     }
 
 }
